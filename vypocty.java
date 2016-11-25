9149335fd675733d93110211a601b249c2b0874c
MaranVaran <marek.varmuza@seznam.cz>
2016-11-25 19:31:09 +0100

    Delete vypocty.java

--- vypocty.java
+++
@@ -1,4 +0,0 @@
-double delkaVektoru (double a1, double b1, double a2, double b2, double a3, double b3) {
-        dv = Math.sqrt(Math.pow((a1 - b1),2) + Math.pow((a2 - b2),2) + Math.pow((a3 - b3),2));
-        return dv;
-    }
